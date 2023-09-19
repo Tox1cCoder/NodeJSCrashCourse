336f17c154ffefb11df210744728d6be650fb92e
Nguyen Duy Thai <92375607+Tox1cCoder@users.noreply.github.com>
2023-09-19 23:49:11 +0700

    Updated handlebars

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const handlebars = require('express-handlebars');
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
 
+const path = require("path");
 const app = express();
 const port = 3000;
 
 //HTTP logger
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 //Template engine
-app.engine("handlebars", handlebars.engine)
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.engine(
+    "hbs",
+    handlebars.engine({
+        extname: ".hbs",
+    })
+);
+app.set("view engine", "hbs");
 
-app.get('/', (req, res) => {
-    res.render('home');
+app.set("views", path.join(__dirname, "resources", "views"));
+
+//Route
+app.get("/", (req, res) => {
+    res.render("home");
+});
+app.get("/news", (req, res) => {
+    res.render("news");
 });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+// 127.0.0.1 - localhost:3000
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
